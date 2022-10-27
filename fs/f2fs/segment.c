f7246775b940cbd0a7e84fa300a996a62ccff37f
Yangtao Li <frank.li@vivo.com>
2022-10-27 18:24:46 +0800

    f2fs: fix return val in f2fs_start_ckpt_thread()
    Return PTR_ERR(cprc->f2fs_issue_ckpt) instead of -ENOMEM;
    
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -714,7 +714,6 @@ int f2fs_create_flush_cmd_control(struct f2fs_sb_info *sbi)
 {
 	dev_t dev = sbi->sb->s_bdev->bd_dev;
 	struct flush_cmd_control *fcc;
-	int err = 0;
 
 	if (SM_I(sbi)->fcc_info) {
 		fcc = SM_I(sbi)->fcc_info;
@@ -738,8 +737,9 @@ init_thread:
 	fcc->f2fs_issue_flush = kthread_run(issue_flush_thread, sbi,
 				"f2fs_flush-%u:%u", MAJOR(dev), MINOR(dev));
 	if (IS_ERR(fcc->f2fs_issue_flush)) {
-		err = PTR_ERR(fcc->f2fs_issue_flush);
-		kvfree(fcc);
+		int err = PTR_ERR(fcc->f2fs_issue_flush);
+
+		kfree(fcc);
 		SM_I(sbi)->fcc_info = NULL;
 		return err;
 	}
